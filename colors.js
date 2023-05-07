e138156c8887841192d234888564fb53f8649ca5
evanmcfarland <evanmcfarland.aa@gmail.com>
2023-05-07 13:38:08 -0400

    All custom color references in tailwind.config.file.

---
+++ colors.js
@@ -0,0 +1,108 @@
+// module.exports = {
+//     bg202123: "#3f5d46",  // "#202123",
+//     bg343541: "#3f5d46",  // "#343541",
+//     bg40414F: "#3f5d46",  // "#40414F",
+//     bg444654: "#3f5d46",  // "#444654",
+//     white: "#FFFFFF",
+//     black: "#3f5d46",
+//     // Add other custom colors
+//   };
+  
+
+
+
+
+
+
+
+
+module.exports = {
+  // Background colors
+  'bg-white': '#FFFFFF',
+  'bg-black': '#000000',
+  'bg-202123': '#202123',
+  'bg-343541': '#343541',
+  'bg-40414F': '#40414F',
+  'bg-444654': '#444654',
+  'bg-neutral-100': '#F5F3F0',
+  'bg-gray-200': = '#EDEBE9',
+  'bg-neutral-300': '#E8E4DC',
+  'bg-blue-500': '#A1D1C9',
+  'bg-gray-50': '#F8F7F5',
+  'bg-gray-500': '#A9A5A2',
+
+  // Text colors
+  'text-black': '#000000',
+  'text-white': '#FFFFFF',
+  'text-gray-500': '#A9A5A2',
+  'text-gray-800': '#524F4C',
+  'text-neutral-800': '#524F4C',
+  'text-neutral-700': '#6D6966',
+  'text-neutral-900': '#3B3936',
+  'text-neutral-200': '#EDEBE9',
+  'text-neutral-300': '#D1CECB',
+  'text-neutral-400': '#BFBEBB',
+  'text-neutral-500': '#A9A5A2',
+  'text-green-500': '#B0D9C5',
+  'text-red-500': '#E6A8A2',
+  'text-blue-500': '#A1D1C9',
+
+  // Border colors
+  'border-black': '#000000',
+  'border-white': '#FFFFFF',
+  'border-gray-300': '#D1CECB',
+  'border-neutral-200': '#EDEBE9',
+  'border-neutral-300': '#D1CECB',
+  'border-neutral-400': '#BFBEBB',
+  'border-neutral-500': '#A9A5A2',
+  'border-neutral-600': '#8E8C88',
+  'border-neutral-800': '#524F4C',
+  'border-neutral-900': '#3B3936',
+
+  // Dark mode colors
+  'dark-bg-white': '#242220',               //'#242220', Main Chatbot Backgouound Lightmode
+  'dark-text-white': '#D1CECB',
+  'dark-text-gray-400': '#8E8D8A',
+  'dark-text-green-400': '#8DB49F',
+  'dark-text-red-400': '#C1857D',
+  'dark-border-neutral-100': '#BFBEBB',
+  'dark-border-neutral-400': '#8E8D8A',
+  'dark-border-neutral-600': '#757370',
+  'dark-border-neutral-700': '#6D6966',
+  'dark-border-neutral-800': '#524F4C',
+  'dark-border-opacity-50': 'rgba(255, 255, 255, 0.5)',
+  'dark-border-gray-900': '#3B3936',
+  'dark-border-none': 'none',
+
+  // Hover colors
+  'hover-text-neutral-100': '#F5F3F0',
+  'hover-text-neutral-200': '#EDEBE9',
+  'hover-text-neutral-300': '#D1CECB',
+  'hover-text-neutral-400': '#BFBEBB',
+  'hover-text-neutral-900': '#3B3936',
+  'hover-text-gray-300': '#D1CECB',
+  'hover-text-gray-400': '#BFBEBB',
+  'hover-text-gray-700': '#6D6966',
+  'hover-bg-343541-90': 'rgba(52, 53, 65, 0.9)',
+  'hover-bg-gray-500-10': 'rgba(169, 165, 162, 0.1)',
+  'hover-bg-neutral-100': '#F5F3F0',
+  'hover-bg-neutral-200': '#EDEBE9',
+  'hover-bg-neutral-300': '#D1CECB',
+  'hover-bg-gray-500': '#A9A5A2',
+  'hover-bg-blue-600': '#93C6B9',
+  'hover-bg-none': 'none',
+  'hover-bg-transparent': 'transparent',
+
+// Opacity colors
+'opacity-50': 'rgba(255, 255, 255, 0.5)',
+'opacity-70': 'rgba(255, 255, 255, 0.7)',
+
+};
+
+
+// text-sidebar
+// text-black/50
+// dark:text-white/50
+// border-white/20
+// hover:bg-[#343541]/90
+// hover:bg-gray-500/10
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,50 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'white': '#fcfff5',    // Homepage full background, input text bar, user responses in chat, regenerate response button.
+        'black': '#000',     // Shading over the app when in settings.
+        '202123': '#a03ee0',
+        '343541': '#a03ee0',
+        '40414F': '#a03ee0',       // IDK   
+        '444654': '#a03ee0',         // IDK
+        'neutral-100': '#F5F3F0', //  Middle Header
+        'gray-200': '#a03ee0',             // IDK
+        'neutral-300': '#E8E4DC',              //  Page bottom button.
+        'blue-500': '##db3db6',           //  IDK
+        'gray-50': '#e6f1ec',         // Bot response background.
+        'gray-500': '#A9A5A2',
+      },
+      textColor: {
+        'black': '#000000',
+        'white': '#FFFFFF',
+        'gray-500': '#A9A5A2',
+        'gray-800': '#524F4C',
+        'neutral-800': '#524F4C',
+        'neutral-700': '#6D6966',
+        'neutral-900': '#3B3936',
+        'neutral-200': '#EDEBE9',
+        'neutral-300': '#D1CECB',
+        'neutral-400': '#BFBEBB',
+        'neutral-500': '#A9A5A2',
+        'green-500': '#B0D9C5',
+        'red-500': '#E6A8A2',
+        'blue-500': '#A1D1C9',
+      },
+      borderColor: {
+        'black': '#000000',
+        'white': '#FFFFFF',
+        'gray-300': '#D1CECB',
+        'neutral-200': '#EDEBE9',
+        'neutral-300': '#D1CECB',
+        'neutral-400': '#BFBEBB',
+        'neutral-500': '#A9A5A2',
+        'neutral-600': '#8E8C88',
+        'neutral-800': '#524F4C',
+        'neutral-900': '#3B3936',
+      },
+    },
   },
   variants: {
     extend: {
@@ -16,3 +59,25 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/typography')],
 };
+
+// Not yet accounted for
+// bg-[#202123]
+// dark:bg-[#202123]
+// dark:bg-[#343541]
+// dark:bg-[#40414F]
+// dark:bg-[#444654]
+// bg-opacity-50
+// bg-[#343541]/90
+// bg-gradient
+// bg-none
+// bg-[#202123]
+// bg-[#343541]
+// dark:bg-opacity-50
+// text-black/50
+// dark:text-white/50
+// text-sidebar
+// border-white/20
+// dark:border-netural-400
+// border-0
+// border-collapse
+// focus:border-neutral-100
